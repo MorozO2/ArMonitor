cd2f2555304bedecb6b2c1e91d8576a01a49434e
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-09-21 15:57:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/armonitor/ArActivity.java
@@ -0,0 +1,32 @@
+package com.example.armonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ArActivity extends AppCompatActivity {
+
+    private Button toMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+
+        toMsg = findViewById(R.id.toMsg);
+        toMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMsgActivity();
+            }
+        });
+    }
+
+    private void openMsgActivity() {
+        Intent openMsgView = new Intent(this, MainActivity.class);
+        startActivity(openMsgView);
+    }
+}
---
+++ app/src/main/java/com/example/armonitor/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.armonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button toAr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        toAr = findViewById(R.id.toAr);
+        toAr.setOnClickListener(new View.OnClickListener() { //BUTTON FOR OPENING AR ACTIVITY
+            @Override
+            public void onClick(View v) {
+                openArActivity();
+            }
+        });
+    }
+
+    //METHOD FOR OPENING AR ACTIVITY
+    private void openArActivity() {
+        Intent openArView = new Intent(this, ArActivity.class);
+        startActivity(openArView);
+    }
+}
