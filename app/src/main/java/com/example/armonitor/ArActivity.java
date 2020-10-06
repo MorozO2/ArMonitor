570978c85174c883554c862074a6522002564ce4
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-10-06 09:30:20 +0300

    Doesn't not crash when switching to ar activity anymore

--- app/src/main/java/com/example/armonitor/ArActivity.java
+++ app/src/main/java/com/example/armonitor/ArActivity.java
@@ -1,24 +1,33 @@
 package com.example.armonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentActivity;
 import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.ar.core.ArCoreApk;
 
 
 public class ArActivity extends AppCompatActivity {
 
     private static final String TAG = ArActivity.class.getSimpleName();
     private Button toMsg;
+    private GLSurfaceView surfaceView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
 
+        surfaceView = findViewById(R.id.surfaceview);
+
+        
         toMsg = findViewById(R.id.toMsg);
         toMsg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,4 +41,5 @@ public class ArActivity extends AppCompatActivity {
         Intent openMsgView = new Intent(this, MainActivity.class);
         startActivity(openMsgView);
     }
+
 }
