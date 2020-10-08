4b28826626149c40ff1ee6e754426ab8b7cf1d40
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-10-08 08:45:46 +0300

    Minor changes

--- app/src/main/java/com/example/armonitor/ArActivity.java
+++ app/src/main/java/com/example/armonitor/ArActivity.java
@@ -1,16 +1,15 @@
 package com.example.armonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 import android.content.Intent;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLES20;
+
 import android.os.Bundle;
 
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.ar.core.ArCoreApk;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -51,16 +50,16 @@ public class ArActivity extends AppCompatActivity implements GLSurfaceView.Rende
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-
+        GLES20.glClearColor(1f, 0f, 0f, 1.0f);
     }
 
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-
+        GLES20.glViewport(0, 0, width, height);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
-
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
     }
 }
